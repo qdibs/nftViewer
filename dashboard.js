be01f71fd815d7bf239ed1af05762429a7c5c475
Qdibs <72762489+qdibs@users.noreply.github.com>
2022-03-03 15:29:20 -0800

    Add files via upload

---
+++ dashboard.js
@@ -0,0 +1,12 @@
+/* globals Chart:false, feather:false */
+// const currentUser = Moralis.User.current();
+
+
+(function () {
+  'use strict'
+
+  feather.replace({ 'aria-hidden': 'true' })
+
+  // Graphs
+  var ctx = document.getElementById('myChart')
+})()
