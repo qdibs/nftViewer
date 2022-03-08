607d14c633eaf4ef74952ca9e695cc44ed61db75
Qdibs <72762489+qdibs@users.noreply.github.com>
2022-03-08 10:24:49 -0500

    Update main.js

--- main.js
+++ main.js
@@ -108,7 +108,7 @@ getNFTs = async () => {
     let tableOfNFTs = document.querySelector('#tableOfNFTs');
 
     if(nfts.result.length > 0) {
-        nfts.result.forEach(n => {
+        nfts.result.for(n => {
            let metadata = JSON.parse(n.metadata); 
            let content = `
            <div class="card col-md-3">
